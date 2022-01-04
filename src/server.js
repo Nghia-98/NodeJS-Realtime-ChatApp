0220e11fc0c0d4b803eca071ef5bbde6f85b6e80
Nghia-98 <hoangnghia.binhthuan@gmail.com>
2022-01-04 21:08:48 +0700

    Connect & create new document in MongoDB

--- src/server.js
+++ src/server.js
@@ -1,15 +1,39 @@
 import express from 'express';
+require('colors');
+import dotenv from 'dotenv';
+
+import connectDB from './config/connectDB';
+import ContactModel from './models/contact.model';
 
 let app = express();
 
 app.use(express.json());
 
-const port = 8080;
+dotenv.config();
+
+// Connect to Database
+connectDB();
+
+const PORT = process.env.PORT || 8080;
 
 app.get('/', (req, res) => {
   res.send('<h1> Server Nodejs Chat App is running now!!! <h1/>');
 });
 
-app.listen(port || 8080, () => {
-  console.log('Sever is running on port ', port);
+app.get('/test-DB', async (req, res) => {
+  try {
+    let item = { userId: 'abc', contactId: 'cba' };
+
+    const newContact = new ContactModel(item);
+    await newContact.save();
+    console.log(':::: New Contact', newContact);
+
+    res.status(201).json(newContact);
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Sever is running on port ${PORT}`.yellow.bold);
 });
